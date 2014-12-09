217dee8cc6e8b0087c2c2a0cb24f727d6d5c8630
John <j@jtgi.me>
2014-12-09 03:19:01 -0800

    dear diary, one day i wrote sqrt())

--- MathPrimitives/MathPrimitives.java
+++ MathPrimitives/MathPrimitives.java
@@ -1,6 +1,30 @@
 
 public class MathPrimitives {
 
+	/*
+	 * The definition of a square root of n is any
+	 * such number x, such that x*x = n. Put simply:
+	 *
+	 * if x*x = n then x = sqrt(n)
+	 * solving for x: x/n = x
+	 *
+	 * Finding the square root of a number
+	 * is an optimization problem, we simply approach
+	 * the true square root of a number until satisfied
+	 * with our precision. Therefore we must define some
+	 * function that gradually converges towards the
+	 * the true square root of n. Since a fixed amount
+	 * of precision is often good enough in many occasions
+	 * we provide a delta `delta` to allow our guestimate
+	 * to be +- a delta of the true sqrt(n).
+	 *
+	 * Heron's Method works in the following manner, we
+	 * make a guess x, and take the average of x and n/x.
+	 * In this way, we are guaranteed to get a number closer
+	 * to that of x. Our guesses will approach the true
+	 * square root quadratically until reaching within
+	 * our delta.
+	 */
     public static double sqrt(double n, double delta) {
         double x = n;
 
